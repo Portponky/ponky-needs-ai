b3d5efa72133dbc337e31b3751125488313fa0cd
Portponky <portponky@gmail.com>
2025-03-22 13:38:42 +0000

    Basic project outline

---
+++ src/register_types.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <godot_cpp/core/class_db.hpp>
+
+void initialize_utility_ai(godot::ModuleInitializationLevel p_level);
+void uninitialize_utility_ai(godot::ModuleInitializationLevel p_level);
+
