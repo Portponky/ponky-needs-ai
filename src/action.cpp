a741bd86426b419de13a9cf62eefd08560dd28cb
Portponky <portponky@gmail.com>
2025-03-24 11:34:39 +0000

    Add basic action properties

--- src/action.cpp
+++ src/action.cpp
@@ -6,11 +6,56 @@ using namespace godot;
 
 void Action::_bind_methods()
 {
+    // advert as property
+
+    ClassDB::bind_method(D_METHOD("set_active", "active"), &Action::set_active);
+    ClassDB::bind_method(D_METHOD("get_active"), &Action::get_active);
+
+    ADD_PROPERTY(PropertyInfo(Variant::BOOL, "active"), "set_active", "get_active");
+
+    ClassDB::bind_method(D_METHOD("set_spatial_weight", "weight"), &Action::set_spatial_weight);
+    ClassDB::bind_method(D_METHOD("get_spatial_weight"), &Action::get_spatial_weight);
+
+    ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "spatial_weight", PROPERTY_HINT_RANGE, "0.0,2.0,or_greater"), "set_spatial_weight", "get_spatial_weight");
+}
+
+void Action::set_advert(const godot::TypedDictionary<godot::String, float>& advert)
+{
+    m_advert = advert;
+    UtilityServer::get_singleton()->action_set_advert(m_rid, m_advert);
+}
+
+godot::TypedDictionary<godot::String, float> Action::get_advert() const
+{
+    return m_advert;
+}
+
+void Action::set_active(bool active)
+{
+    m_active = active;
+    // notification system should handle activity too
+}
+
+bool Action::get_active() const
+{
+    return m_active;
+}
+
+void Action::set_spatial_weight(float weight)
+{
+    m_spatial_weight = weight;
+    UtilityServer::get_singleton()->action_set_spatial_weight(m_rid, m_spatial_weight);
+}
+
+float Action::get_spatial_weight() const
+{
+    return m_spatial_weight;
 }
 
 Action::Action()
 {
     m_rid = UtilityServer::get_singleton()->create_action();
+    // add object instance id as callback source
 }
 
 Action::~Action()
--- src/action.h
+++ src/action.h
@@ -2,16 +2,31 @@
 
 #include <godot_cpp/classes/node2d.hpp>
 
+#include <godot_cpp/variant/typed_dictionary.hpp>
+
 class Action : public godot::Node2D
 {
     GDCLASS(Action, godot::Node2D);
 
     godot::RID m_rid;
 
+    godot::TypedDictionary<godot::String, float> m_advert;
+    bool m_active{true};
+    float m_spatial_weight{1.0f};
+
 protected:
     static void _bind_methods();
 
 public:
+    void set_advert(const godot::TypedDictionary<godot::String, float>& advert);
+    godot::TypedDictionary<godot::String, float> get_advert() const;
+
+    void set_active(bool active);
+    bool get_active() const;
+
+    void set_spatial_weight(float weight);
+    float get_spatial_weight() const;
+
     Action();
     ~Action();
 };
