f8249f589b019738e1f1a56abcd8fbb31266cf99
Portponky <portponky@gmail.com>
2025-04-01 18:20:57 +0100

    Improve toilet efficiency

--- test/objects/Toilet.gd
+++ test/objects/Toilet.gd
@@ -10,14 +10,14 @@ func plan(person: Person, action: Action) -> void:
 		person.do_start_use(self)
 		person.do_wait(6.0, 8.0)
 		person.do_stop_use(self)
-		person.do_loiter(1.0)
+		person.do_loiter(0.4)
 		person.do_reward(action.advert)
 		
 		action.active = false
 		%Timer.start()
 	elif action == %Beacon:
 		person.do_walk_to(global_position + Vector2(0, 20))
-		person.do_loiter(1.0)
+		person.do_loiter(0.2)
 
 
 func _on_timer_timeout() -> void:
